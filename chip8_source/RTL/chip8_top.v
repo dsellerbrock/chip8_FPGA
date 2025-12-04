c8e2a7d7698d2baa5a262c19d7c061f990a8341d
Daniel Ellerbrock <dsellerbrock@gmail.com>
2025-12-03 21:30:16 -0600

    adding instructions

--- chip8_source/RTL/chip8_top.v
+++ chip8_source/RTL/chip8_top.v
@@ -37,6 +37,7 @@ module ch8_top(
     reg [7:0]  sound;
     reg [7:0]  gen_reg [15:0];
     reg [15:0] stack [31:0];
+    reg [4:0] stack_ptr;
     // -------------------------
     // Intermediate Values
     // -------------------------   
@@ -49,9 +50,9 @@ module ch8_top(
     reg [3:0] N;
     reg [7:0] NN;
     reg [11:0] NNN;
+    reg [31:0] screen_buffer [0:63];
 
-
-    
+    reg [7:0] ascii_254_char = 8'hFE;
     assign next_pc = PC + 2;
 
     // -------------------------
@@ -68,14 +69,27 @@ module ch8_top(
 
     
     initial begin
+        //forever begin
         for (i = 0; i < 4096; i = i + 1)
             ram[i] = 8'hFF;
 
         $readmemh("chip8_source\\memory\\snake.mem", ram);
 
         // optional display for simulation
-         for (i = 0; i < 16; i = i + 1)
+         for (i = 0; i <  16; i = i + 1)
             $display("RAM[%0d] = %h", i, ram[i]);
+
+        
+
+        // Print the character directly using the %c format specifier (SystemVerilog)
+        // or by letting the terminal interpret the raw byte data (Verilog)
+        //$write("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n");
+        //$write("%c%c%c", 8'hE2, 8'h96, 8'hA0);  // UTF-8 for █
+        //$write("%c%c%c", 8'hE2, 8'h96, 8'hA0);  // UTF-8 for █
+        //$write("%c%c%c", 8'hE2, 8'h96, 8'hA0);  // UTF-8 for █
+        //$display(); // Add a newline after
+        //$display("The character for ASCII 254 is: \xFE");
+        //end
     end
 
      always @(posedge clk) begin
@@ -98,12 +112,13 @@ module ch8_top(
     
     always @(posedge clk) begin
         if (reset == 1) begin
-                I = 0;
-               PC = 0;
-            delay = 0;
-            sound = 0; 
-            reset = 0;
-            fdx_state = 2'b01;
+                I <= 0;
+               PC <= 0;
+            delay <= 0;
+            sound <= 0; 
+            reset <= 0;
+            stack_ptr <= 0;
+            fdx_state <= 2'b01;
         end
         else begin
             case (fdx_state)
@@ -124,12 +139,116 @@ module ch8_top(
                     fdx_state <= EXECUTE;
                 end
             
-                /*EXECUTE: begin
+                EXECUTE: begin
                     case(instr)
-                    
+                    4'h0: begin
+                        case (NNN)
+                            12'h0E0: begin
+                               for 
+                            end
+                            12'h0EE: begin
+                                PC <= stack[stack_ptr-1];
+                                stack_ptr <= stack_ptr - 1;
+                            end
+                            default: begin
+                            end
+                        endcase
+                    end
+                    4'h1: begin
+                        PC <= NNN;
+                    end
+                    4'2: begin
+                        stack[stack_ptr] <= PC;
+                        stack_ptr <= stack_ptr + 1;
+                        PC<=NNN;     
+                    end
+                    4'h3: begin
+                        if(gen_reg[X] == NN ) begin
+                            PC <= PC + 2;
+                        end
+                    end
+                    4'h4: begin
+                        if(gen_reg[X] != NN) begin
+                            PC <= PC + 2;
+                        end
+                    end
+                    4'h5: begin
+                        if(gen_reg[X] == gen_reg[Y]) begin
+                            PC <= PC + 2;
+                        end
+                    end
+                    4'h6: begin
+                        gen_reg[X] <= NN;
+                    end
+                    4'h7: begin
+                        gen_reg[X] <= gen_reg[X] + NN;
+                    end
+                    4'h8: begin
+                        case (N) begin
+                            4'h0: begin
+                                gen_reg[X] <= gen_reg[Y];
+                            end
+                            4'h1: begin
+                                gen_reg[X] <= gen_reg[X] | gen_reg[Y];
+                            end
+                            4'h2: begin
+                                gen_reg[X] <= gen_reg[X] & gen_reg[Y];
+                            end
+                            4'h3: begin
+                                gen_reg[X] <= gen_reg[X] ^ gen_reg[Y];
+                            end
+                            4'h4: begin
+                                gen_reg[X] <= gen_reg[X] + gen_reg[Y];
+                            end
+                            4'h5: begin
+                                gen_reg[X] <= gen_reg[X] - gen_reg[Y];
+                            end
+                            4'h6: begin
+                                gen_reg[X] <= gen_reg[Y] >> 1 ;
+                                gen_reg[4'hf] <= gen_reg[0];  
+                            end
+                            4'h7: begin
+                               gen_reg[X] <= gen_reg[Y] - gen_reg[X];
+                            end
+                            4'hE: begin
+                               gen_reg[X] <= gen_reg[Y] << 1 ;  
+                               gen_reg[4'hf] <= gen_reg[15];  
+                            end
+                            default: begin
+                            end
+                        endcase
+                        
+                    end
+                    4'h9: begin
+                        if(gen_reg[X] != gen_reg[Y]) begin
+                            PC <= PC + 2;
+                        end
+                    end
+                    4'hA: begin
+                        I <= NNN;
+                    end
+                    4'hB: begin
+                        PC <= NNN;
+                    end
+                    4'hC: begin
+                        PC <= NNN;
+                    end
+                    4'hD: begin
+                        PC <= NNN;
+                    end
+                    4'hE: begin
+                        PC <= NNN;
+                    end
+                    4'hF: begin
+                        PC <= NNN;
+                    end
+                    default: begin
+                    //function not implemented
+                    end
+
                     endcase
                     fdx_state <= FETCH;
-                end*/
+                end
                 default: fdx_state = INVALID;
             endcase
         end
